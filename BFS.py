75f9b4412a301373fd89f72f2daa7a2d90196ca0
rortunox <roberto.o.med@gmail.com>
2024-11-16 19:09:01 -0300

    primer repositorio

---
+++ BFS.py
@@ -0,0 +1,21 @@
+def BFS(source, graph):
+    P = {}  # Padres
+    Q = []  # cola (o pila en caso de DFS)
+
+    Q.append(source)
+    P[source] = None
+
+    while Q:
+        v = Q.pop(0)#Q.pop() para que sea una pila (DFS)
+        for node in graph[v]:
+            if node not in P.keys():
+                P[node] = v
+                Q.append(node)
+
+    return P
+
+
+G = {'A': ['B', 'C'], 'B': ['A', 'C', 'D'], 'C': ['A', 'B', 'E'], 'D': ['B'], 'E': ['C', 'F'], 'F': ['E'], 'T': ['N']}
+
+resultado = BFS('A', G)
+print(resultado)
